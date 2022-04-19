9affbac690c770e2e8f27d28e125191721d3935c
Gabry17 <gbianchi528@gmail.com>
2022-04-19 15:49:19 +0200

    eseguito snack 2

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -1,4 +1,13 @@
 //SNACK2
 //creare array vuoto
+let number = [];
 //chiedere 6 numeri all utente
-//i numeri dispari vanno inseriti nell array
+for(let i = 0; i < 6; i ++){
+    const addNumber = parseInt(prompt('inserisci numero'));
+    console.log(addNumber);
+    //i numeri dispari vanno inseriti nell array
+    if (addNumber % 2 === 1) {
+        number.push(addNumber);
+    }
+}
+console.log(number);
