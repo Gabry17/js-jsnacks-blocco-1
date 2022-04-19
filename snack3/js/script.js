216d56e802a4bac8ec63859e1183aff674b4b420
Gabry17 <gbianchi528@gmail.com>
2022-04-19 15:59:45 +0200

    eseguito snack 3

--- snack3/js/script.js
+++ snack3/js/script.js
@@ -1,3 +1,18 @@
 //generare i primi 10 numeri
-//sommarli tutti quanti
+
+let somma = 0;
+
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+    
+    //sommarli tutti quanti
+    somma = somma + i;
+
+
+}
+
 //fare la media di tutti
+const media = somma / 10;
+console.log(somma, media);
+
+
